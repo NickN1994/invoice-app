47c5c4aebea009771ffd2c97bf9f0482eb8603f8
NickN1994 <nickneggers@hotmail.com>
2024-01-17 20:13:11 +0100

    pre test context

--- src/pages/Factuur.jsx
+++ src/pages/Factuur.jsx
@@ -1,9 +1,199 @@
+import calcSubtotal from "../helpers/calcSubtotal.js";
+import calcTotal from "../helpers/calcTotal.js";
+import calcPriceUnitHelper from "../helpers/calcPriceUnitHelper.js";
+import Formfield from "../components/FormField.jsx";
+import {useEffect, useState} from "react";
 
 
-function Factuur () {
+
+function Factuur() {
+
+    // product op invoice
+    const [products, setProducts] = useState([]);
+    const [amount, setAmount] = useState(1);
+    const [productName, setProductName] = useState('');
+    const [vat, setVat] = useState(21);
+    const [price, setPrice] = useState(0);
+
+    useEffect(() => {
+        console.log(products)
+    }, [products]);
+
+    let idCounter = 1;
+
+    const addProduct = () => {
+        const newProduct = {
+            key: idCounter++,
+            name: productName,
+            amount: amount,
+            vat: vat,
+            priceNoVat: calcPriceUnitHelper(price, vat),
+            price: price
+        }
+        // console.log(products);
+        setProducts([...products, newProduct]);
+        // console.log(products);
+
+        setProductName('');
+        setAmount(1);
+        setVat(21);
+        setPrice(0);
+    }
+
+
 
     return (
-        <>  </>
+        <div>
+
+            <div className="addProduct">
+
+                <Formfield
+                    labelName="Aantal"
+                    type="number"
+                    // placeholder="1"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+
+                <Formfield
+                    labelName="Naam product of dienst"
+                    type="text"
+                    placeholder="Naam product of dienst"
+                    value={productName}
+                    onChange={(e) => setProductName(e.target.value)}
+                />
+
+                <section>
+                    <label htmlFor="BtwPercentage">Btw percentage</label>
+                    <select
+                        name="vat"
+                        id="vat"
+                        value={vat}
+                        onChange={(e) => setVat(parseInt(e.target.value))}
+                    >
+                        <option value="21">21%</option>
+                        <option value="9">9%</option>
+                        <option value="0">0%</option>
+                    </select>
+                </section>
+
+                <Formfield
+                    labelName="Prijs"
+                    type="number"
+                    placeholder="0"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+
+                <button onClick={addProduct}>Product toevoegen</button>
+            </div>
+
+
+            <div>
+                <section>
+                    <h3>Factuuradres:</h3>
+                    <p>{customerName}</p>
+                    <p>{customerStreet}</p>
+                    <p>{customerPostcode}</p>
+                    <p>{customerCity}</p>
+                </section>
+                <section>
+                    <h3>{companyName}</h3>
+                    <p>{street}</p>
+                    <p>{postcode}</p>
+                    <p>{city}</p>
+                    <p>{phonenumber}</p>
+                    <p>IBAN: {bankNumber}</p>
+                    <p>Kvk: {companyName}</p>
+                    <p>Btw nummer: {vatNumber}</p>
+                </section>
+                <section>
+                    <p><strong>Datum:</strong> {date}</p>
+                    <p><strong>Factuurnummer:</strong> {invoiceNumber}</p>
+                </section>
+                <section>
+                    {/* PRODUCTEN OP FACTUUR    */}
+                    <div>
+                        <p><strong>Beschrijving</strong></p>
+                        {products && products.map((product) => {
+                            return (
+                                <>
+                                    <p>{product.name}</p>
+                                </>
+                            )
+                        })}
+                    </div>
+                    <div>
+                        <p><strong>Aantal</strong></p>
+                        {products && products.map((product) => {
+                            return (
+                                <>
+                                    <p>{product.amount}</p>
+                                </>
+                            )
+                        })}
+                    </div>
+
+                    <div>
+                        <p><strong>Prijs excl. Btw</strong></p>
+                        {products && products.map((product) => {
+                            return (
+                                <>
+                                    <p>{(product.priceNoVat * product.amount).toFixed(2)}</p>
+                                </>
+                            )
+                        })}
+                    </div>
+
+                    <div>
+                        <p><strong>Btw</strong></p>
+                        {products && products.map((product) => {
+                            return (
+                                <>
+                                    <p>{product.vat}%</p>
+                                </>
+                            )
+                        })}
+                    </div>
+
+                    <div>
+                        <p><strong>Prijs incl. Btw</strong></p>
+                        {products && products.map((product) => {
+                            return (
+                                <>
+                                    <p>€{(product.price * product.amount).toFixed(2)}</p>
+                                </>
+                            )
+                        })}
+                    </div>
+                </section>
+
+                {/* HELPER AANMAKEN NOG OM SUBTOTAAL ETC. TE BEREKENEN*/}
+
+                <div>
+                    <section>
+                        <p><strong>Subtotaal: </strong></p>
+                        <p>€{calcSubtotal(products)}</p>
+                    </section>
+                    <section>
+                        <p><strong>Btw bedrag: </strong></p>
+                        <p>€{(calcTotal(products) - calcSubtotal(products)).toFixed(2)}</p>
+                    </section>
+                    <section>
+                        <p><strong>Totaal incl. btw: </strong></p>
+                        <p>€{calcTotal(products)}</p>
+                    </section>
+                </div>
+
+                <div>
+                    <p><strong>Graag het bedrag overmaken op bankrekeningnummer {bankNumber} en het factuurnummer erbij
+                        vermelden.</strong></p>
+                </div>
+
+            </div>
+
+        </div>
+
     )
 }
 
--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,18 +1,10 @@
-import {useEffect, useState} from "react";
+import {createContext, useState} from "react";
 import Formfield from "../components/FormField.jsx";
-import calcPriceUnitHelper from "../helpers/calcPriceUnitHelper.js";
-import calcSubtotal from "../helpers/calcSubtotal.js";
-import calcTotal from "../helpers/calcTotal.js";
+import {Link} from "react-router-dom";
 
+export const moveFormContext = createContext({});
 
-function Form() {
-
-    // LOGO UPLOADEN? FACTUURGEGEVENS => naam, adres, GEGEVENS BEDRIJF => naam, adres, telefoonnr, bankrekeningnr, kvk nr, btw nr
-// datum, factuurnummer
-// amount, beschrijving product, prijs per unit, btw percentage, subtotaal, btw bedrag, totaal
-// webite voor algemene voorwaarden
-// binnen hoeveel dagen betalen
-// tnv
+function Form(  ) {
 
     // bedrijfsgegevens
     const [date, setDate] = useState('');
@@ -32,41 +24,6 @@ function Form() {
     const [customerPostcode, setCustomerPostcode] = useState('');
     const [customerCity, setCustomerCity] = useState('');
 
-    // product op invoice
-    const [products, setProducts] = useState([]);
-    const [amount, setAmount] = useState(1);
-    const [productName, setProductName] = useState('');
-    const [vat, setVat] = useState(21);
-    const [price, setPrice] = useState(0);
-
-    useEffect(() => {
-        console.log(products)
-    }, [products]);
-
-    let idCounter = 1;
-
-    // HIER NOG KEY MEEGEVEN
-    const addProduct = () => {
-        const newProduct = {
-            key: idCounter++,
-            name: productName,
-            amount: amount,
-            vat: vat,
-            priceNoVat: calcPriceUnitHelper(price, vat),
-            price: price
-        }
-        // console.log(products);
-        setProducts([...products, newProduct]);
-        // console.log(products);
-
-        setProductName('');
-        setAmount(1);
-        setVat(21);
-        setPrice(0);
-    }
-
-    // const total = calcTotal(products);
-
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log("De gegevens worden in de factuur gezet ");
@@ -76,9 +33,9 @@ function Form() {
         <div className="outer-container">
             <div className="inner-container">
                 <h1>Vul hieronder de gevraagde gegevens in om een factuur te maken</h1>
-                <h2>Je bedrijfsgegevens</h2>
                 <form onSubmit={handleSubmit}>
                     <div className="companyInfo">
+                    <h2>Je bedrijfsgegevens</h2>
                         <Formfield
                             labelName="Je bedrijfsnaam"
                             type="text"
@@ -195,156 +152,8 @@ function Form() {
                             onChange={(e) => setCustomerCity(e.target.value)}
                         />
                     </div>
-                    <button onClick={handleSubmit}>Zet gegevens op factuur</button>
+                    <button onClick={handleSubmit}><Link to="/factuur">Zet gegevens op factuur</Link></button>
                 </form>
-
-                <div className="addProduct">
-
-                    <Formfield
-                        labelName="Aantal"
-                        type="number"
-                        // placeholder="1"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                    />
-
-                    <Formfield
-                        labelName="Naam product of dienst"
-                        type="text"
-                        placeholder="Naam product of dienst"
-                        value={productName}
-                        onChange={(e) => setProductName(e.target.value)}
-                    />
-
-                    <section>
-                        <label htmlFor="BtwPercentage">Btw percentage</label>
-                        <select
-                            name="vat"
-                            id="vat"
-                            value={vat}
-                            onChange={(e) => setVat(parseInt(e.target.value))}
-                        >
-                            <option value="21">21%</option>
-                            <option value="9">9%</option>
-                            <option value="0">0%</option>
-                        </select>
-                    </section>
-
-                    <Formfield
-                        labelName="Prijs"
-                        type="number"
-                        placeholder="0"
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-
-                    <button onClick={addProduct}>Product toevoegen</button>
-                </div>
-
-                    {/*     DE FACTUUR      */}
-                    {/*     KLANT GEGEVENS  */}
-                    <div>
-                        <section>
-                            <h3>Factuuradres:</h3>
-                            <p>{customerName}</p>
-                            <p>{customerStreet}</p>
-                            <p>{customerPostcode}</p>
-                            <p>{customerCity}</p>
-                        </section>
-                        <section>
-                            <h3>{companyName}</h3>
-                            <p>{street}</p>
-                            <p>{postcode}</p>
-                            <p>{city}</p>
-                            <p>{phonenumber}</p>
-                            <p>IBAN: {bankNumber}</p>
-                            <p>Kvk: {companyName}</p>
-                            <p>Btw nummer: {vatNumber}</p>
-                        </section>
-                        <section>
-                            <p><strong>Datum:</strong> {date}</p>
-                            <p><strong>Factuurnummer:</strong> {invoiceNumber}</p>
-                        </section>
-                        <section>
-                            {/* PRODUCTEN OP FACTUUR    */}
-                            <div>
-                                <p><strong>Beschrijving</strong></p>
-                                {products && products.map((product) => {
-                                    return (
-                                        <>
-                                            <p>{product.name}</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-                            <div>
-                                <p><strong>Aantal</strong></p>
-                                {products && products.map((product) => {
-                                    return (
-                                        <>
-                                            <p>{product.amount}</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-
-                            <div>
-                                <p><strong>Prijs excl. Btw</strong></p>
-                                {products && products.map((product) => {
-                                    return (
-                                        <>
-                                            <p>{(product.priceNoVat * product.amount).toFixed(2)}</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-
-                            <div>
-                                <p><strong>Btw</strong></p>
-                                {products && products.map((product) => {
-                                    return (
-                                        <>
-                                            <p>{product.vat}%</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-
-                            <div>
-                                <p><strong>Prijs incl. Btw</strong></p>
-                                {products && products.map((product) => {
-                                    return (
-                                        <>
-                                            <p>€{(product.price * product.amount).toFixed(2)}</p>
-                                        </>
-                                    )
-                                })}
-                            </div>
-                        </section>
-
-                        {/* HELPER AANMAKEN NOG OM SUBTOTAAL ETC. TE BEREKENEN*/}
-
-                        <div>
-                            <section>
-                            <p><strong>Subtotaal: </strong></p>
-                            <p>€{calcSubtotal(products)}</p>
-                            </section>
-                            <section>
-                                <p><strong>Btw bedrag: </strong></p>
-                                <p>€{(calcTotal(products) - calcSubtotal(products)).toFixed(2)}</p>
-                            </section>
-                            <section>
-                                <p><strong>Totaal incl. btw: </strong></p>
-                                <p>€{calcTotal(products)}</p>
-                            </section>
-                        </div>
-
-                {/*    EINDE FACTUUR    */}
-
-
-                </div>
-
-
             </div>
         </div>
     )
